33a9d585197eb57ee7f996b8d68877eb91684e17
Tasnim <87016275+Tasnim-Pricila@users.noreply.github.com>
2024-10-24 14:04:44 +0600

    infinite scrolling added

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 const Loading = () => {
-    console.log(11)
   return (
     <div className="flex justify-center items-center h-screen">
       <p className="text-red-600">Loading........</p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,50 +3,82 @@
 import Image from "next/image";
 import { Movie } from "./types/movie";
 import Loading from "./loading";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
 
 export default function Home() {
-  const fetchMovies = async () => {
+  const fetchMovies = async ({ pageParam }: { pageParam: number }) => {
     const res = await fetch(
-      `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`
+      `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&page=${pageParam}`
     );
     const data = await res.json();
-    return data.results;
+    return data;
   };
 
-  const { data: movies, isLoading } = useQuery<Movie[]>({
+  const {
+    data: movies,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+  } = useInfiniteQuery({
     queryKey: ["movies"],
     queryFn: fetchMovies,
+    getNextPageParam: (lastPage) => lastPage.page + 1,
+    initialPageParam: 1,
   });
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (
+        window.innerHeight + window.scrollY >= document.body.offsetHeight &&
+        hasNextPage
+      ) {
+        fetchNextPage();
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [hasNextPage, fetchNextPage]);
+
+  if (isFetching && !isFetchingNextPage) return <Loading />;
+  console.log(movies);
+
   return (
-    <div className="grid grid-cols-6 gap-4 mt-4">
-      {isLoading && <Loading />}
-      {movies?.map((movie: Movie) => (
-        <div
-          key={movie.id}
-          className="max-w-xs rounded overflow-hidden shadow-lg bg-white hover:shadow-xl transition-shadow duration-200"
-        >
-          <Image
-            src={`https://image.tmdb.org/t/p/original${movie?.poster_path}`}
-            alt={movie?.title}
-            width={300}
-            height={300}
-            className="w-full h-48 object-cover"
-          />
-          <div className="p-4">
-            <h2 className="text-lg font-semibold text-gray-800">
-              {movie?.title}
-            </h2>
-            <p className="text-gray-600 mt-1 text-sm">
-              Release Date: {movie?.release_date}
-            </p>
-            <p className="text-gray-600 text-sm">
-              Rating: {movie?.vote_average}
-            </p>
-          </div>
-        </div>
-      ))}
+    <div>
+      <div className="grid grid-cols-8 gap-4 mt-4">
+        {movies?.pages?.map((page) =>
+          page?.results?.map((movie: Movie) => (
+            <div
+              key={movie.id}
+              className="max-w-xs rounded overflow-hidden shadow-lg bg-white hover:shadow-xl transition-shadow duration-200"
+            >
+              <Image
+                src={`https://image.tmdb.org/t/p/original${movie?.poster_path}`}
+                alt={movie?.title}
+                width={300}
+                height={300}
+                className="w-full h-48 object-cover"
+                placeholder="blur"
+                blurDataURL={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              />
+              <div className="p-4">
+                <h2 className="text-lg font-semibold text-gray-800">
+                  {movie?.title}
+                </h2>
+                <p className="text-gray-600 mt-1 text-sm">
+                  Release Date: {movie?.release_date}
+                </p>
+                <p className="text-gray-600 text-sm">
+                  Rating: {movie?.vote_average}
+                </p>
+              </div>
+            </div>
+          ))
+        )}
+      </div>
+      {isFetchingNextPage && <p>Loading more movies...</p>}
     </div>
   );
 }
