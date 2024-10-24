1841089b699b0d4b525311da8fe2c21491c3989f
Tasnim <87016275+Tasnim-Pricila@users.noreply.github.com>
2024-10-24 13:04:05 +0600

    popular movie list shown

---
+++ src/app/loading.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Loading = () => {
+    console.log(11)
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <p className="text-red-600">Loading........</p>
+    </div>
+  );
+};
+
+export default Loading;
